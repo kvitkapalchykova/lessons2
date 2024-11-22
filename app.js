6777ede33f5fbbf5d216ed4e68ab01739ebdbf53
kvitka <hoobarivka@gmail.com>
2024-11-22 23:23:10 +0200

    умова2

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 const money = 10;
 
-if (money > 50) {
+if (money >= 10) {
     console.log('Можна купити наш товар');
-} else if (money > 55) {
+} else if (money > 5) {
     console.log('Купили міні товар');
 } else {
     console.log('Не вистачає коштів');
