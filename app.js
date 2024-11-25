758894931ffcc9223f505331a9125f9489fcce36
kvitka <hoobarivka@gmail.com>
2024-11-25 19:20:32 +0200

    if-else

--- app.js
+++ app.js
@@ -1,11 +1,6 @@
-const isAdmin = true;
-const canWrite = true;
-
-console.log(`Системний файл ${isAdmin && canWrite}`);
-console.log(`Звичайний файл ${isAdmin || canWrite}`);
-console.log(`Інвертуємо права адміна ${!isAdmin}`);
-
-const isEdited = true;
-const isSuperAdmin = true;
-
-console.log(`Системний файл з редагуванням ${isAdmin && canWrite && (!isEdited || isSuperAdmin)}`);
+const wife = 'Kvitka';
+if(wife === 'Kvitka'){
+    console.log('good!');
+}else{
+    console.log('not good!');
+}
