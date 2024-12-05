d242a0e910edb58216fc41abcfaab6d487eb4210
kvitka <hoobarivka@gmail.com>
2024-12-05 19:55:21 +0200

    alert,prompt,confirm

--- app.js
+++ app.js
@@ -1,16 +1,8 @@
-/*Визначте дві змінні:admin і name.
-Запишіть рядок "Джон" у змінній name. 
-Скопіюйте значення зі змінної name до admin.
-Виведіть на екран значення admin, використовуючи функцію alert(має показати "Джон").*/
+let name = prompt("моє ім'я?", "Квітка");
+alert(name);
 
-let admin, name;
-name = "Джон";
-admin = name; //чому перекреслює??
-alert(admin);
+let age = prompt("скільки мені років?", "39");
+alert(age);
 
-/*мій варіант розвʼязання:
-let admin;
-let name = "Джон";
-admin = name;
-
-console.log(admin);*/
+let isBoss = confirm("Я тут головна?");
+alert(isBoss);
