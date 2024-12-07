7bb93d8ce0045761f9df284f38cff7f645a93e98
kvitka <hoobarivka@gmail.com>
2024-12-07 11:09:18 +0200

    майже безпечна ф-я калькулятор

--- app.js
+++ app.js
@@ -1,5 +1,21 @@
-function calculator(x, y){
-    console.log(x + y, x - y, x * y, x / y);
+function calculator(x, y, z){
+    if(z === '+'){
+        console.log(x + y)
+    }else if(z === '-'){
+        console.log(x - y)
+    }else if(z === '*'){
+        console.log(x * y)
+    }else if(z === '/'){
+        console.log(x / y)
+    }else{
+        console.log('Шукай помилку!')
+    }
 }
 
-calculator(1, 2);
+calculator(1, 1, '+');
+calculator(2, 1, '-');
+calculator(2, 2, '*');
+calculator(1, 1, '/');
+calculator(2, 3, '')
+
+console.log(parseInt('ljg123'))
