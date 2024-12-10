6845a2182ec9531550cdc3e3f4e143eb5ec56638
kvitka <hoobarivka@gmail.com>
2024-12-10 23:30:10 +0200

    функції та її параметри

--- app.js
+++ app.js
@@ -1,54 +1,24 @@
-let a = 0;
+let age = 39;
 
-if(a === '0'){ //варіанти 1, 0, -3
-    console.log('Правильно!');
-}else{
-    console.log('Неправильно!');
+function adultWebsite(a){
+    if(a > 18){
+        console.log('Доступ відкритий');
+    }else{
+        console.log('Немає доступу!')
+    }
 }
+adultWebsite(40);//Доступ відкритий
+adultWebsite(20);//Доступ відкритий
 
-if(a > 0){
-    console.log('Правильно!');
-}else{
-    console.log('Неправильно!');
+age = 16;
+console.log(age);
+adultWebsite(age);//Немає доступу!
+
+
+let name
+function myName(myname){
+    name = myname;
+    return name;
 }
-
-if(a <= 0){
-    console.log('Правильно!');
-}else{
-    console.log('Неправильно!');
-}
-
-if(a !== 0){
-    console.log('Неправильно!');
-}else{
-    console.log('Правильно!');
-}
-
-if(a === 'test'){ //'test','тест',3
-    console.log('Правильно!');
-}else{
-    console.log('Неправильно!');
-}
-
-
-let x = "0";
-switch (x){
-    case 0:
-        console.log("off");
-        break;
-    case 1:
-        console.log("On");
-        break;
-    default:
-        console.log("Значення не знайдено");//тому що строге порівняння
-}
-
-const secretNumber = '7';
-
-if (Number(secretNumber) === 7){
-    console.log('Вгадав строго');
-}
-
-if(secretNumber == 7){
-    console.log('Вгадав не строго'); 
-}
+myName('Kvitka');
+console.log(name);
