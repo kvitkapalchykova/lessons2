b9d2718d137ad916223c6424b7ab0aeebe4b3eff
kvitka <hoobarivka@gmail.com>
2024-12-15 22:39:10 +0200

    розробка функції cleaning

--- app.js
+++ app.js
@@ -1,37 +1,39 @@
-//зовнішні змінні в функціях взаємодіють:
-let userName = 'Квітка';
-
-function showMessage1(){
-    let message = 'Привіт,' + userName;
-    return message;
-}
-showMessage1();//Привіт,Квітка
-
-//функція має повний доступ до зовнішньої змінної та може змінювати її:
- let userName1 = 'Світлана';
-
-function showMessage(){
-    userName1 = 'Сергій';//змінено зовнішню змінну
-
-    /*let userName = 'Сергій' -якщо оголосити локальну змінну(let) у самій функції,то вона має перевагу та виконається всередині, а зовнішня змінна не зміниться(залишиться Світлана)*/ 
-
-    let message = 'Здоровенькі були, ' + userName1;
-    return message;
-    return userName1; //проігноровано бо дія припиняється і наступний код не виконується
-    let userName; //проігноровано
+// washing
+// sweeping
+// cleaning
+function washing(rag = true){
+    if(rag){
+        return 'добре';
+    }else{
+        return 'треба придбати ганчірку'
+    }
 }
 
-// console.log(userName1);//Світлана до оголошення функції 
-// showMessage();//Здоровенькі були, Сергій
-// console.log(showMessage());
+function sweeping(milf = true){
+    if (milf){
+        return 'супер';
+    }else{
+        return 'треба придбати віник'
+    }
+}
 
-// document.querySelector('p').innerText = userName1;
+function cleaning(){
+    
+    if (washing() === true){
+        return 'мию підлогу';
+    }else if(sweeping() === true){
+        return 'підмітаю';
+    }else if(washing() && sweeping()){
+        return 'повне прибирання';
+    }else{
+        return "я нечемна,чоловік наб`є дупу "
+    }
 
-let p = document.querySelector('p')//спосіб вивести результат в документ
-p.innerText = showMessage()
+}
+console.log(cleaning());//повне прибирання
+console.log(washing());
+console.log(sweeping());
 
-let h1 = document.querySelector('h1')
-h1.innerText = showMessage1()
-
-console.log(h1)
-// console.log(userName1);//Сергій після оголошення функції
+// функція, яка збирає вси решта
+// main() 
+// або cleaning()
