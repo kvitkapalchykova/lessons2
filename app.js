d9e01085914785021654f9ca2a85d4d29a3c64d2
kvitka <hoobarivka@gmail.com>
2024-12-18 15:25:08 +0200

    1 урок: Цикли

--- app.js
+++ app.js
@@ -1,66 +1,54 @@
-//watering
-// washing
-// sweeping
-// cleaning
+const months = ['січень','лютий','березень','квітень','травень','червень','липень','серпень','вересень','жовтень','листопад','грудень'];
 
-function watering(flowers = false, water = false){
-    if(flowers && water){
-        return true;
-    }else{
-        return false;
-    }
+//forEach() -метод перебрати масив
+months.forEach(month => console.log(month)); 
 
-    //варіант тернарного оператора умови:
-  // return flowers && water === true ? true : false
+console.log('array.forEach')
+
+months.forEach(function(month){
+    console.log(month)
+})
+
+console.log('for: ')
+
+let i = 100
+// це різні перемінні 
+
+// цикл (перебор)
+for(let i = 0; i<12; i++){
+    // це різні перемінні 
+    console.log(months[i])
 }
 
-function washing(rag = false){
-    //тернарний оператор може бути важкий для розуміння
-    return rag === true ? true : false
+{
+    let i = 200 // це різні перемінні 
 }
 
-function sweeping(milf = false){
-    //для лаконічності коду ліпше використовувати булевий оператор
-    if (milf){
-        return true;
-    }else{
-        return false;
-    }
+console.log('add number')
+
+for(let i = 0; i<=11; i++){
+    // це різні перемінні 
+    console.log(i, months[i])
 }
 
-function cleaning(rag, milf, flowers, water){
-//використовуємо константу для швидшого виконання коду
-    const wat = watering(flowers,water);
-    const wash = washing(rag);
-    const sweep = sweeping(milf);
+console.log('кількість елемнетів масиву: ', months.length)
 
-    // console.log('debug:', wat, wash, sweep)
-
-    switch(true){
-    case (!wash && !sweep && wat):
-        return 'тільки квіти полила';
-    case (wash && !sweep && wat):
-        return 'мию підлогу та поливаю квіти';
-    case (sweep && !wash && wat):
-        return 'підмітаю та поливаю квіти';
-    case (wash && sweep && !wat):
-        return 'повне прибирання, але квіти не политі';
-    case (wash && sweep && wat):
-        return 'все зроблено'
-    case (!wash && !sweep && !wat):
-        return "я нечемна,чоловік наб`є дупу "
-    }
+for(let i = 0; i<months.length; i++){
+    // це різні перемінні 
+    console.log(months[i])
 }
-console.log(cleaning(false,false,true,true));//тільки квіти полила
-console.log(cleaning(true,false,true,true));//мию підлогу та поливаю квіти
-console.log(cleaning(false,true,true,true));//підмітаю та поливаю квіти
-console.log(cleaning(true,true,false,true));//повне прибирання,але квіти не политі,бо немає квітів
-console.log(cleaning(true,true,true,false))//повне прибирання, квіти не полила, бо немає води
-console.log(cleaning(true,true,true,true))//все зроблено
-console.log(cleaning(false,false,false,false));//я нечемна, чоловік наб'є дупу
-console.log(watering(true,true))//якщо не передати параметри,то буде автоматично false,оскільки два параметра мають значення по замовчуванню false
-console.log(cleaning())
 
-// функція, яка збирає вси решта
-// main() 
-// або cleaning()
+console.log('перший елемент масива: ', months[0])
+
+// костил - обмежив довжину масива до 3 елементів. якщо поствивит 0, то масив станепорожнім 
+months.length = 3
+
+console.log(months)
+
+months.length = 15
+
+console.log(months)
+
+console.log(months[5])
+
+console.log(typeof months)
