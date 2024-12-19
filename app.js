bd1118ebe4d371235ea786ae42192bb31418d5d5
kvitka <hoobarivka@gmail.com>
2024-12-19 12:41:36 +0200

    3 урок: повторення цикли

--- app.js
+++ app.js
@@ -2,68 +2,26 @@ const months = ['січень','лютий','березень','квітень',
 
 const number = [10,20,30,40,50,60,70,80,90,100]
 
-console.dir(months)
 
-function random(x = 0,y=0){
-    return Math.random(x * y)
-}
-
-console.log(random(2,3))
-console.log(random(2,3))
-console.log(random(2,3))
-
-
-months.forEach(month => console.log(month))
-
-console.log('')
-
-months.forEach(function(month){
-    console.log(month)
-})
-
-console.log('')
-
-/* 
-function trulala(item){
-    console.log(item)
-}
-
-months.forEach(trulala(item)) 
-*/
+//спроба по памʼяті
 
 number.forEach(num => console.log(num))
 
-console.log('')
-
 number.forEach(function(num){
-    console.log(num * 2)
+    console.log(num);
 })
 
-// функція, яка додає до ціни 10%
-function plus10persent(price){
-    return price / 100 * 10 + price
+for(let i = 0; i < number.length; i++ ){
+    console.log(number[i]);
 }
 
-console.log('for: ')
-
-for(let i = 0; i < number.length; i++){
-    console.log(plus10persent(number[i]))
+let i = 20;
+while(i > 0){
+    console.log(number[i])
+    i--
 }
-
-console.log('forEach: ')
-
-number.forEach(num => console.log(plus10persent(num)))
-
-console.log('while: ')
-
-let i = 0
-while(i < number.length){
-    console.log(plus10persent(number[i]))
-    i++
-}
-
-console.log('for of: ')
 
 for(const num of number){
-    console.log(plus10persent(num))
+    console.log(num)
 }
+
