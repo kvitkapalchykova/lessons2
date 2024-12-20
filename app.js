c4e8cc68fa6c1d7ba310190ec91235a356ab0735
kvitka <hoobarivka@gmail.com>
2024-12-20 21:16:11 +0200

    урок 5: метод splise,який додає та видаляє єлементи в масиві в будь-яке вкзане місце

--- app.js
+++ app.js
@@ -1,36 +1,33 @@
 const numbers = [10,20,30,40,50,60,70,80,90,100]
 
-// додати в кінець
-numbers.push(110)
+// this  - вказує на саме ʼцейʼ об'єкт
+// the | a
 
-// жорстко додано елемент у конкретний індекс
-numbers[20] = 150
+// map, set, weekmap, weekset 
+// prototype 
 
-//жорстко додано елемент у конкретний індекс 
-numbers[30] = 200
-
-for(const number of numbers){
-    console.log(number)
+/* 
+function trulala(){
+    return this
 }
 
-//видаляє та повертає крайнє значення масиву
-let pop = numbers.pop()
-console.log('крайній елемент масива: ', pop)
+console.log(typeof trulala(), this)
+ */
 
-numbers.forEach(number => console.log(number))
+console.log(numbers)
+//видаляє вкінці, після 5-го, 3 елемента 
+numbers.splice(5, 3)
 
-// видаляє та повертає перше значення масиву
-let shift = numbers.shift()
-console.log('перший елемент масива:' , shift)
+console.log(numbers)
+//додає після 6-го такі значення
+numbers.splice(6, 0, 200, 210, 220, 230)
 
-for(let i = 0; i < numbers.length; i++){
-    console.log(numbers[i])
-}
+console.log(numbers)
+//додає в кінець елементи
+numbers.splice(-1, 0, 300, 310, 320)
 
+console.log(numbers)
+//додає в кінець елементи
+numbers.splice(numbers.length, 0, 400, 410, 420, 430)
 
-// додає елемент на початок масива
-numbers.unshift(0)
-
-numbers.forEach(function(number){
-    console.log(number)
-})
+console.log(numbers)
