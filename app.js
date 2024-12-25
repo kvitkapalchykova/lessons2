d0cea9d936cc76cdcd7acc871740d8c2503602c0
kvitka <hoobarivka@gmail.com>
2024-12-25 21:57:13 +0200

    функція main()2

--- app.js
+++ app.js
@@ -1,35 +1,34 @@
-function toGo1(road1){
-    if(road1 = 1){
+function toLeft(go){
+    if(go === 1){
         return 1;
     }else{
         return 0;
     }
 }
 
-function toGo2(road2){
-    if(road2 = 2){
+function toRight(go){
+    if(go === 2){
         return 2;
     }else{
         return 0;
     }
 }
 
-function main(road1, road2){
-    const go1 = toGo1(road1)
-    const go2 = toGo2(road2)
+function main(go){
 
-    if(go1 && !go2){
-        return 'йдемо направо';
-    }else if( !go1 && go2){
-        return 'йдемо наліво';
-    }else{
-        return 'стоїмо';
+    const go1 = toLeft(go)
+    const go2 = toRight(go)
+
+    switch(go){
+        case 1: return 'йдемо направо';
+        case 2: return 'йдемо наліво';
+        default: 'стоїмо';
     }
 
 }
-console.log(main(1,0))
-console.log(main(0,2))
-console.log(main(0,0))
+console.log(main(1))
+console.log(main(2))
+console.log(main(3))
 
-console.log(toGo1())//1
-console.log(toGo2())//2
+console.log(toLeft(1))//1
+console.log(toRight(2))//2
