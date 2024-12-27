fbbabde821c2e4df1a9dba9e711c3e87223aaea3
kvitka <hoobarivka@gmail.com>
2024-12-27 10:56:13 +0200

    функція main()7

--- app.js
+++ app.js
@@ -35,11 +35,23 @@ function main(go){
     //відсортовуємо значення 0 в масиві та кладемо вже потрібне число в масив
     for(let i = 0; i < array.length; i++){
         if(array[i] !== 0){
-            // result.push(array[i])
+            //result.push(array[i])
 
             //присвоюємо змінній result елемент масиву array
             result = array[i]
         }
+        //метод, який фільтрує непотрібні елементи масиву 
+       array.filter(function(arr){
+        if(array[i] !== 0){
+            console.log(array[i])
+        }
+
+       })
+     // array.filter(arr => array[i] !== 0)
+      //console.log(array[i])
+        
+
+
     }
 
     //console.log(result)
@@ -50,7 +62,6 @@ function main(go){
         case 2: return 'йдемо наліво'
         default: return 'стоїмо'
     }
-
 }
 
 console.log(main(1))
