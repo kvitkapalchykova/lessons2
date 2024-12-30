c5532c2305658130d4a66846e910eb18bf497b25
kvitka <hoobarivka@gmail.com>
2024-12-30 17:41:16 +0200

    домашнє завдання проста функція

--- app.js
+++ app.js
@@ -1,44 +1,22 @@
-//функція "йдемо направо" яка повертає 1 або 0 :
-function toLeft(go){
-    if(go === 1){
-        return 1;
-    }else{
-        return 0;
+function getRating(a){
+    switch(a){
+        case 1:
+            return 'дуже погано'
+        case 2:
+            return 'погано'
+        case 3:
+            return 'слабенько'
+        case 4:
+            return 'задовільно'
+        case 5:
+            return 'найвищий рівень'
+        default:
+            return 'спробуйте ще '
     }
 }
-
-//функція "йдемо наліво" яка повертає значення 2 або 0 :
-function toRight(go){
-    if(go === 2){
-        return 2;
-    }else{
-        return 0;
-    }
-}
-
-//функція, яка приймає значення цих функцій та виконується в залежності від умов :
-function main(go){
-
-    //створюємо масив 
-    const array = []
-
-    //добавляємо в масив значення функцій за допомогою методу
-    array.push(toLeft(go))
-    array.push(toRight(go))
-
-    //метод, який фільтрує непотрібні елементи масиву та створює новий масив
-    let newArray = array.filter(item => item !==0)
-
-    console.log(newArray)
-
-    //використовуємо swich, щоб виконати певний блок коду в залежності від заданої умови
-    switch(newArray[0]){
-        case 1: return 'йдемо направо'
-        case 2: return 'йдемо наліво'
-        default: return 'стоїмо'
-    }
-}
-
-console.log(main(1))
-console.log(main(2))
-console.log(main(0))
+console.log(getRating(1))
+console.log(getRating(2))
+console.log(getRating(3))
+console.log(getRating(4))
+console.log(getRating(5))
+console.log(getRating(9))
