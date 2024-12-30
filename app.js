1b65cab3fbedaafe1680f5c1b6d0a04c386c3599
kvitka <hoobarivka@gmail.com>
2024-12-30 23:27:50 +0200

    функція та методи масиву в ній

--- app.js
+++ app.js
@@ -1,22 +1,38 @@
-function getRating(a){
-    switch(a){
-        case 1:
-            return 'дуже погано'
-        case 2:
-            return 'погано'
-        case 3:
-            return 'слабенько'
-        case 4:
-            return 'задовільно'
-        case 5:
-            return 'найвищий рівень'
-        default:
-            return 'спробуйте ще '
-    }
+const colors = [];
+colors.push('жовтий')
+colors.push('блактиний')
+colors.push(1)
+colors.push(2)
+colors.push(3)
+colors[5] = 4
+colors[6] = 5
+
+const array = []
+array.push(1)
+array.push(2)
+array.push(3)
+array.push('trulala')
+
+const alphabet = ['a', 'b', 'c', 'd']
+
+// функція повинна міняти масив, який ти туди передаєш 
+function moveFirstToLast(arr) {
+    //видаляємо перший елемент масиву та повертаємо його
+    const shift = arr.shift()
+    //додаємо цей видалений елемент в кінець масиву
+    arr.push(shift)
 }
-console.log(getRating(1))
-console.log(getRating(2))
-console.log(getRating(3))
-console.log(getRating(4))
-console.log(getRating(5))
-console.log(getRating(9))
+
+moveFirstToLast(alphabet)
+console.log(alphabet)
+moveFirstToLast(alphabet)
+console.log(alphabet)
+moveFirstToLast(array)
+console.log(array)
+
+// console.log(colors.push(getColors()))
+/*let shift = colors.shift()
+console.log(colors)
+
+colors.push(shift)*/
+// console.log(colors)
