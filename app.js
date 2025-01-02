8030c94d34cb573257a88dea26d8df67f4751346
kvitka <hoobarivka@gmail.com>
2025-01-02 13:18:57 +0200

    повторення по памʼяті функція та методи масивів

--- app.js
+++ app.js
@@ -1,30 +1,29 @@
-// Масиви :
-// можна створити пустий масив, а потім додати значення
 const fruits = []
-fruits[0] = 'Banana'
-fruits[1] = 'Orange'
-fruits[2] = 'Apple'
-fruits[3] = 'Mango'
+fruits.push('Banana')
+fruits.push('Orange')
+fruits.push('Apple')
+fruits.push('Mango')
 
+const number = [1, 2, 3, 4, 5]
+
+function fn(arr, flag = true){
+    if(flag){
+        const result = arr.shift()
+        arr.push(result)
+    }else{
+        const result = arr.pop()
+        arr.unshift(result)
+    }
+}
+
+fn(fruits)
 console.log(fruits)
 
-// показує кількість елементів масиву
-let length = fruits.length 
-console.log(fruits.length)
+fn(number)
+console.log(number)
 
-// показує перший елемент масиву
-let fruit = fruits[0]
-console.log(fruits[0])
-
-// міняємо перший елемент на інший
-fruits[0] = 'Lemon'
+fn(fruits,false)
 console.log(fruits)
 
-// доступ до останнього елементу
-let fruit1 = fruits[fruits.length - 1]
-console.log(fruits[fruits.length -1])
-
-// додає новий елемент в масив (вкінці)
-fruits[fruits.length] = 'Kiwi'
-
-console.log(fruits)
+fn(number, 0)
+console.log(number)
