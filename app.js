987f228662607aba782e5ab2839549f00d32346a
kvitka <hoobarivka@gmail.com>
2025-01-03 12:02:27 +0200

    види функцій та їх відмінності

--- app.js
+++ app.js
@@ -1,50 +1,23 @@
-const cars = []
-cars.push('BMW')
-cars.push('Audi')
-cars.push('Opel')
+const f = true
 
-const numbers = [1, 2, 3, 4, 5]
+// function expression (анонімна функція)
+let fn = function(arr, flag = true){
 
-function fn(arr, flag = true){
-    if(flag){
-        const result = arr.pop()
-        arr.unshift(result)
-    }else{
-        const result = arr.shift()
-        arr.push(result)
-    }
 }
 
-fn(cars)
-console.log(cars)
 
-fn(numbers)
-console.log(numbers)
-
-fn(cars, '')
-console.log(cars)
-
-fn(numbers, 0)
-console.log(numbers)
-
-// Цикли :
-cars.forEach(car => console.log(car))
-
-numbers.forEach(function(num){
-    console.log(num)
-})
-
-let i = 0
-while( i < cars.length ){
-    console.log(cars[i])
-    i ++
+function return4(x,y){
+    return x + y
 }
 
-for( i = 0; i < 5; i++){
-    console.log(numbers[i])
-}
+// function declaraion (іменована функція)
+// фунція колбек (тестувальна)
+function testReturn4(func, x, y){
+    return func(x=0,y=0) * 2
+} 
 
-for(const car of cars){
-    console.log(car)
-}
-    
+const resultTest = testReturn4(return4)
+
+console.log(resultTest)
+
+// this(диис) посилання на саму себе
