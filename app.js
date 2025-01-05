8c56703e5e37f2d693319ab40d077e5f4997b869
kvitka <hoobarivka@gmail.com>
2025-01-05 15:57:38 +0200

    вправа зі сайту - функція вищого порядку Сallback

--- app.js
+++ app.js
@@ -1,68 +1,28 @@
-let fruits = ['апельсин','грушка', 'ківі', 'яблучко']
-const colors = ['Помаранчевий', 'Жовтий', 'Зелений', 'Червоний']
-const seasons = ['Зима', 'Весна', 'Літо', 'Осінь']
-const tastes = ['Кислий', 'Терпкий', 'Солодкий', 'Гіркий']
-
-// вирізаємо перший елемент у першого масива і вставляємо на початок другого
-function f1(arr1, arr2){
-    const result = arr1.shift()
-    arr2.unshift(result.toUpperCase())
+function add(a, b){
+    return a + b;
 }
 
-// вирізаємо крайній елемент у першого масива і вставляємо на кінець другого
-function f2(arr1, arr2){
-    arr2.push(arr1.pop())
+function subtact(a, b){
+    return a - b;
 }
 
-// вирізаємо перший елемент у першого масива і вставляємо на кінець другого
-function f3(arr1, arr2){
-    arr2.push(arr1.shift())
+function power(a, b){
+    return a ** b;
 }
 
-// вирізаємо крайній елемент у першого масива і вставляємо на початок другого
-function f4(arr1, arr2){
-    arr2.unshift(arr1.pop())
+// функція вищого порядка Callback 
+function calculate(a, b, fn){
+    // Атрибут name використовується щоб задати назву параметру 
+    console.log(fn.name);
+    const res = fn(a, b);
+    return res;
 }
 
-f1(fruits, colors)
-console.log(fruits)
-console.log(colors)
+let res = calculate(3, 5, add)
+console.log(res)
 
-f2(seasons, tastes)
-console.log(seasons)
-console.log(tastes)
+res = calculate(3, 5, subtact)
+console.log(res)
 
-f3(fruits, colors)
-console.log(fruits)
-console.log(colors)
-
-f4(seasons, tastes)
-console.log(seasons)
-console.log(tastes)
-
-// також можна виконати функцію, використавши тільки один масив
-f4(seasons, seasons)
-console.log(seasons)
-
-// цикли для масиву  fruits та array :
-// використовуємо метод, який повертає значення,перетворене у верхній регістр (велиими літерами)
-const array = []
-for(let i = 0; i < colors.length; i++){
-    array.unshift(colors[i].toUpperCase())
-}
-console.log(array)
-
-// властивість видалити усі елементи масиву
-array.length = 0
-console.log(array)
-
-for(const el of fruits){
-    array.push(el.toUpperCase())
-}
-
-console.log(array)
-
-// Метод map() надає можливість створити новий масив на основі елементів існуючого, застосовуючи до кожного елемента певну функцію.
-fruits = fruits.map(el => el.toUpperCase())
-
-console.log(fruits)
+res = calculate(3, 5, power)
+console.log(res)
