a6e050be60f07ab7f2e097f231748c1aae5085a4
kvitka <hoobarivka@gmail.com>
2025-01-09 21:17:31 +0200

    приклади методів forEach(), map(), filter()

--- app.js
+++ app.js
@@ -1,18 +1,44 @@
-// функція вищого порядку 
-// повернення функції з функції
+// метод forEach()
+const score = [5, 10, 0, 15];
 
-function power(pow) {
-    return function(num) {
-        return num**pow;
-    }
+score.forEach((scoreEl, i) => {
+    console.log(`Раунд ${i + 1}: ${scoreEl}`);
+})
+
+// Раунд 1: 5
+// Раунд 2: 10
+// Раунд 3: 0
+// Раунд 4: 15
+
+// цикл for/of як перший варіант конвертації долара в гривні
+const transactionInUSD = [10, -7, 50, -10, 100];
+const transactionInUAH = []
+for (const transaction of transactionInUSD) {
+    transactionInUAH.push(transaction * 40);
 }
 
-const powerOfTwo = power(2)
-console.log(powerOfTwo(5)) // 25
-console.log(powerOfTwo(10)) // 100
+console.log(transactionInUSD)
+console.log(transactionInUAH)
 
-const powerOfThree = power(3)
-console.log(powerOfThree(5)) // 125
+// метод map(), який повертає новий масив та виконує певну функцію
+const transactionInUAH2 = transactionInUSD.map(transaction => transaction * 40)
 
-// короткий варіант визову функцій
-console.log(power(2)(5)) // 25
+
+console.log(transactionInUSD)
+console.log(transactionInUAH2)
+
+// перший метод перебрати масив та відібрати позитивні операції з нього, поклавши вновий масив за допомогою for/of
+const operations = [100, -20, 7, -20, 50];
+const positiveOperations = [];
+for (const operation of operations) {
+    if (operation > 0) {
+        positiveOperations.push(operation);
+    }
+}
+console.log(positiveOperations)
+
+// метод filter(),який фільтрує та залишає потрібні нам елементи в новому масиві
+const positiveOperations2 = operations.filter(operation => {
+    return operation > 0;
+})
+console.log(positiveOperations2)
