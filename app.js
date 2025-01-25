9fc3b028184302a3cbe66b2e1b8d5609e726e487
kvitka <hoobarivka@gmail.com>
2025-01-25 11:23:02 +0200

    функція,яка кладе в третій масив по елементу з двох масивів та усі види циклів для виконання

--- app.js
+++ app.js
@@ -1,13 +1,33 @@
-const fruit = [ 'яблучко', 'грушка', 'банан', 'мандарин']
-const vegetables = [ 'бульба', 'морква', 'бурячок', 'цибуля']
+// 'яблучко' -- нульовий елемент (нульовий індекс
+const fruits = [ 'яблучко', 'грушка', 'банан', 'мандарин' ]
+
+console.log('нульовий індекс, перший елемент: ', fruits.indexOf('яблучко')) // 0 (нульовий індекс, перший елемент)
+
+const vegetables = [ 'бульба', 'морква', 'бурячок', 'цибуля' ]
 const shopping = []
 
-function combine ( arr1, arr2, arr3 ) {
+function combine ( arr1, arr2, arrReduce ) {
 
-    for ( let i = 0; i < arr1.length; i++ ) {
-        arr3.push(arr1[i], arr2[i])
+   /* for ( let i = 0; i < arr1.length; i++ ) {
+        arrReduce.push(arr1[i], arr2[i])
+    }*/
+
+   /* let i = 0;
+    while( i < arr1.length ) {
+        arrReduce.push( arr1[i], arr2[i])
+        i++
+    }*/
+
+    for( const el of arr1 ) {
+        arrReduce.push( el, arr2[arr1.indexOf(el)])
     }
-    return shopping
+
+    return arrReduce
 }
-console.log( combine(fruit, vegetables, shopping))
-console.log(shopping)
+
+console.log( combine(fruits, vegetables, shopping))
+
+for(const fruit of fruits){
+    shopping.push(fruit, vegetables[fruits.indexOf(fruit)])
+}
+console.log(shopping)  
