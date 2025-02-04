8f71dc05171714062a07eec73a01413aa3ab2900
kvitka <hoobarivka@gmail.com>
2025-02-04 21:28:56 +0200

    методи з document , альтернатива бібліотеці jQuery

--- app.js
+++ app.js
@@ -14,3 +14,39 @@ log(par)
 log($('p').addClass('blue'))
 log($('p').removeClass('red'))
 
+/* $("p").click(function(){
+  $(this).hide();
+}); */
+
+/*$('strong').hover(function(){
+    $(this).css('color', 'orange');
+})
+
+$('strong').click(function(){
+    $(this).css('color', 'black');
+})*/
+
+// міняє колір на оранж при наведенні миші
+document.querySelectorAll('strong').forEach(el => {
+
+    el.addEventListener('mouseenter', function() {
+        this.style.color = 'orange'
+    })
+})
+
+// міняє колір на чорний при натисканні миші 
+document.querySelectorAll('strong').forEach(el => {
+
+    el.addEventListener('click', function(){
+        this.style.color = 'black'
+    })
+})
+
+// міняє колір на яскраво-зелений при відведенні миші від елементу
+document.querySelectorAll('strong').forEach(el => {
+
+    el.addEventListener('mouseout', function(){
+        this.style.color = 'cyan'
+    })
+
+})
