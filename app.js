237f1c17a783f8e2c959b0519e683af80390f4af
kvitka <hoobarivka@gmail.com>
2025-02-26 22:13:48 +0200

    масив ( Array ) -це особливий вид об"єкта, створений для зберігання та обробки впорядкованих колекцій (елементів)

--- app.js
+++ app.js
@@ -1,66 +1,77 @@
 'use strict'
 
-const user = {
-    name: 'Kvitka',
-    surname: 'Palchycova',
-    age: 39,
-    skills: [
-        'Програмування',
-        'Кулінарія'
-    ],
-    getFullName() {
-        return this.name + ' ' + this.surname;
+const person = {
+    firstName: 'John',
+    lastName: 'Doe',
+    language: '',
+    set(lang) {
+        this.language = lang;
     }
 };
 
-user.sity = 'Харків'
-console.log(user)
-console.log(user.getFullName())
-
-// ствоюємо порожній об'єкт із зазначеними об'єктом прототипом та властивостями
-const user1 = Object.create(user)
-console.log(user1)
-
-// створюємо пустий об'єкт без прототипу
-const user2 = Object.create(null)
-console.log(user2)
-
-// створюємо порожній безпечний об'єкт та додаєм йому властивості 
-const user3 = {}
-Object.defineProperty( user3, 'a', {
-    value: 7,
-    // якщо false === заборона 
-    writable: true, // дозвіл змінювати значення властивості
-    enumerable: true, // доступна властивість у циклах for
-    configurable: true // дозвіл змінювати та видаляти властивості
-} );
-console.log(user3.a) // 7
-user3.a = 10
-console.log(user3.a) // 10
-console.log(user3)
+person.lang = 'en'
+console.log(person.lang)
+console.log(person)
 
 
-// метод, за допомогою якого створюємо властивості , а потім додаємо сеттер і геттер
-const jar = {}
-Object.defineProperties( jar, {
-    'quantity': {
-        value: 0,
-        configurable: true
-    },
-    'cookies': {
-        get() {
-            return this.quantity;
-        },
-        set(value) {
-            if ( value > 0 && value < 10 ) {
-                Object.defineProperty(this, 'quantity', {value});
-            }
-        }
-    }
-});
 
-console.log(jar.cookies)
-jar.cookies = 5
-console.log(jar.cookies)
-//jar.quantity = 10
-console.log(jar.cookies)
+const array = [ 1, 3, 'nums', function(){return 2}, person, null, { a: 'd'}]
+
+console.log(array[3]())
+console.log(array[4].lastName)
+console.log(array[5])
+console.log(array[6].a)
+console.log(array[6]['a'])
+
+console.log(array)
+
+// отримати останні елементи :
+console.log(array[array.length -1])
+console.log(array.at(-1))
+//метод pop() також повертає останній елемент масиву,але також змінює масив, видаляючи його :
+console.log(array.pop())
+
+// додаємо новий елемент в кінець :
+console.log(array.push('Kvitka'))
+//console.log(array[array.length] = 'Kvitka')
+
+// пербір масиву за допомогою циклу :
+for (let arr of array) {
+    console.log(arr)
+}
+
+/*for (let i = 0; i < array.length; i++) {
+    console.log(array[i])
+}*/
+
+const num = [1,2,3,4,5]
+
+// масиви реалізують метод toString, який повертає список елементів розділених комою :
+console.log(String(num))
+
+// в цьому прикладі масив також конвертується в рядок :
+console.log([] + 1) // 1
+console.log([2] + 1) // 21
+console.log([1,2] + 1) // 1,21
+
+
+const styles = ["Jazz", "Blues"]
+
+styles.push("Rock-n-Roll")
+styles[1] = "Classics"
+
+// Замініть значення в середині масиву на “Classics”. Ваш код для пошуку медіанного елемента має працювати для будь-яких масивів непарної довжини.
+styles[Math.floor((styles.length - 1) / 2)] = "Classics";
+
+const elementStyles = styles.shift()
+console.log(elementStyles)
+
+styles.unshift("Rap", "Reggae")
+console.log(styles)
+
+const array1 = ['a', 'b']
+array1.push(function() {
+     return this;
+     // alert(this) ?
+})
+console.log(array1[2]())
