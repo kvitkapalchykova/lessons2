04a2902ab0de31379db425e3d5bd45fb63cf5eb9
kvitka <hoobarivka@gmail.com>
2025-03-23 23:04:23 +0200

    за допомогою методу bind() об"єкт може запозичити метод від іншого об"єкта, також запобігає втраті this

--- app.js
+++ app.js
@@ -4,34 +4,18 @@ const log = console.log
 const audi = {
     make: 'Audi',
     model: 'A3',
-    year: 2021,
     damages: [],
-    addDamage( part,rate ) {
-        log(`У авто ${this.make} ${this.model} ${this.year} додано - пошкодження: ${part}, ступінь: ${rate}`)
-        this.damages.push({
-            part,
-            rate
-        })
+};
+
+const carManipulation = {
+    addDamage(part, rate) {
+        this.damages.push({ part, rate});
+        log(`Додати пошкодження на ${this.make} ${this.model}`)
     }
 }
-audi.addDamage('Капот',1)
 
-const bmw = {
-    make: 'BMW',
-    model: 'X5',
-    year: 2022,
-    damages: [],
-}
+// метод bind, який запозичує метод :
+const addDamageAudi = carManipulation.addDamage.bind(audi)
 
-bmw.addDamage = audi.addDamage;
-//bmw.addDamage('Бампер', 2)
-
-const addDamageFunc = audi.addDamage;
-
-// метод call приймає аргументи окремо :
-addDamageFunc.call(bmw, 'Бампер', 2)
-
-// метод apply приймає аргументи як масив :
-addDamageFunc.apply(bmw, ['Капот', 1])
-addDamageFunc.apply(audi, ['Бампер', 2])
-
+addDamageAudi('Крило', 3)
+log(audi)
