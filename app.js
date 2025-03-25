3f017b41f60de06e5fce2142b021d690f16a9045
kvitka <hoobarivka@gmail.com>
2025-03-25 21:26:16 +0200

    приклади з замиканням

--- app.js
+++ app.js
@@ -1,23 +1,51 @@
 'use strict'
 const log = console.log
 
-// функція скидання пароля :
-function removePassword(reset) {
-    if (reset) {
-        this.password = undefined;
-    } else {
-        this.password = '1';
+// замикання :
+function changeBalanse() {
+    let balanse = 0;
+
+    return function(sum) {
+        balanse += sum;
+        log(`Баланс: ${balanse}`);
     }
 }
 
-const user = {
-    login: 'kvitka.ua',
-    password: '12345'
-};
+const change = changeBalanse()
+change(100) // 100
+change(-50) // 50
+change(200) // 250
 
-log(user)
-// використовуєм bind() щоб примінити метод(функцію) до об'єкту user :
-const resetUserPassword = removePassword.bind(user, true)
-resetUserPassword()
 
-log(user)
+// замикання з об'єктами :
+const userInfo = {
+    balanse: 0,
+    operations: 0,
+    increse(sum) {
+        this.balanse += sum;
+        this.operations++;
+    }
+}
+
+function user() {
+    const userObj = {
+        balanse: 0,
+        operations: 0,
+        increse(sum) {
+            this.balanse += sum;
+            this.operations++; 
+        }
+    };
+    return function() {
+        return userObj;
+    }
+}
+
+const user1 = user()
+user1().increse(100)
+user1().increse(100)
+log(user1())
+
+const user2 = user()
+user2().increse(100)
+log(user2())
