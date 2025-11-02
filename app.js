a581d1edaf8c3500fc9c73453739aee6783e48bb
kvitka <hoobarivka@gmail.com>
2025-11-02 11:20:03 +0200

    Спрощення фунцій - перетворення в стрілочну функцію

--- app.js
+++ app.js
@@ -1,17 +1,19 @@
 const log = console.log
 
-// Перетворення типу :
-log("" + 1 + 0) // "10"
-log("" - 1 + 0) // -1
-log(true + false) // 1
-log(6 / "3") // 2
-log("2" * "3") // 6
-log(4 + 5 + "px") // "9px"
-log("$" + 4 + 5) // "$45"
-log("4" - 2) // 2
-log("4px" - 2) // NaN
-log(" -9 " + 5) // " -9 5"
-log(" -9 " - 5) // -14
-log(null + 1) // 1
-log(undefined + 1) // NaN
-log(" \t \n "- 2) // -2
+function powerOfTwo(num) {
+    return num * num;
+}
+log(powerOfTwo(5))
+
+const poft = (num) => num * num; 
+log(poft(5))
+
+
+function toPower(num, power){
+    const res = num * power;
+    return res;
+}
+log(toPower(2, 3))
+
+const toPowerArrow = (num, power) => num * power
+log(toPowerArrow(2, 3))
