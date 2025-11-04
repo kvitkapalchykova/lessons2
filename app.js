e977e6a3bf566c80aa5e8c9c77f734e7c0263bc6
kvitka <hoobarivka@gmail.com>
2025-11-04 15:54:12 +0200

    Інструкція if() та умовний оператор `?`

--- app.js
+++ app.js
@@ -1,16 +1,49 @@
 const log = console.log
 
-log(true == 1) //true
-log(false == 0) //true
-log(null == 0) //false
-log(null === 0) //false
-log(undefined > 0) //false
-log(undefined < 0) //false
-log(undefined == 0) //false
-log(undefined == null) //true
-log(undefined === null) //false
-log(5 > 4) //true
-log("ананас" > "яблуко") //false
-log("2" > "12") //true
-log(null == "\n0\n") //false
-log(null === +"\n0\n") //false
+let age = 17
+/*let mesage = (age < 3) ? 'Привіт, крихітко!' :
+    (age < 18) ? 'Вітаю!' :
+    (age < 100) ? 'Моє шанування!' :
+    'Який невизначений вік!';
+
+log(mesage)*/
+
+if(age < 3) {
+    log('Привіт, крихітко!'); 
+} else if(age < 18) {
+    log('Вітаю!');
+} else if(age < 100) {
+    log('Моє шанування!');
+} else {
+    log('Який невизначений вік! ');
+}
+
+let nameJavaScript = 'ECMAScript';
+if(nameJavaScript == 'ECMAScript') {
+    log('Правильно!');
+} else {
+    log('Ви не знаєте? ECMAScript!');
+}
+
+let value = 1;
+if(value > 0) {
+    log(1);
+} else if(value < 0) {
+    log(-1);
+} else {
+    log(0);
+} 
+
+// використання умовного оператора замість умови if :
+let a = 2;
+let b = 3;
+let result = (a + b < 4) ? 'Нижче' : 'Вище';
+log(result)
+
+let login = '';
+let mesage = (login == 'Працівник') ? 'Привіт' :
+    (login == 'Директор') ? 'Вітаю' :
+    (login == '') ? 'Немає логіну' : 
+     ' '
+
+log(mesage)     
