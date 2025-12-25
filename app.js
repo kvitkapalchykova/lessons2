49fa37c20841168d89d89ad023eb77b99a852d60
kvitka <hoobarivka@gmail.com>
2025-12-25 17:50:21 +0200

    вправа з об`єктом ``гаманець``

--- app.js
+++ app.js
@@ -2,23 +2,46 @@
 
 const log = console.log
 
-//
-let calculator = {
-    read() {
-        this.a = +prompt('a?', 0);
-        this.b = +prompt('b?', 0);
+const wallet = {
+    balance: 0,
+    operations: [],
+    //метод збільшення балансу:
+    increase: function(sum, reason) {
+        this.balance += sum;
+        this.operations.push({
+            reason: reason,
+            sum: sum
+        })
+        return true;
     },
-    sum() {
-        return this.a + this.b;
+    //метод зменшення балансу:
+    decrease: function(sum, reason) {
+        if (this.balance < sum) {
+            log('Недостатньо коштів');
+            return false;
+        }
+        this.balance -= sum;
+        this.operations.push({
+            reason: reason,
+            sum: -sum
+        })
+        return true;
     },
-    mul() {
-        return this.a * this.b;
+    //показує кількість операцій:
+    getOperationLenght: function() {
+        return this.operations.length;
     }
 };
-calculator.read()
-log( calculator.sum() )
-log( calculator.mul() )
 
+log(wallet.increase(1000, 'Заробітня плата')) // true
+log(wallet.getOperationLenght()) // 1
+//20 Недостатньо коштів
+log(wallet.decrease(2000, 'Купити телефон')) // false
+log(wallet.getOperationLenght()) // 1
+log(wallet.decrease(500, 'Купити морозиво')) // true
+log(wallet.getOperationLenght()) // 2
+log(wallet.balance) // 500
+log(wallet.operations)
 
 
 
