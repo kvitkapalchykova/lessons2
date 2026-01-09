47f8e7e3591b5b5d4da05c81da623815b8709439
kvitka <hoobarivka@gmail.com>
2026-01-09 18:23:45 +0200

    this не є фіксованим,його значення визначається під час виконнання

--- app.js
+++ app.js
@@ -1,48 +1,14 @@
 'use strict';
 
-const log = console.log
+const user = { name: "Квітка" }
+const admin = { name: "Пальчик" }
 
-const wallet = {
-    balance: 0,
-    operations: [],
-    //метод збільшення балансу:
-    increase: function(sum, reason) {
-        this.balance += sum;
-        this.operations.push({
-            reason: reason,
-            sum: sum
-        })
-        return true;
-    },
-    //метод зменшення балансу:
-    decrease: function(sum, reason) {
-        if (this.balance < sum) {
-            log('Недостатньо коштів');
-            return false;
-        }
-        this.balance -= sum;
-        this.operations.push({
-            reason: reason,
-            sum: -sum
-        })
-        return true;
-    },
-    //показує кількість операцій:
-    getOperationLenght: function() {
-        return this.operations.length;
-    }
-};
+function sayHi() {
+    console.log( this.name )
+}
+// використовуємо одну функцію для обох об'єктів : 
+user.f = sayHi
+admin.f = sayHi
 
-log(wallet.increase(1000, 'Заробітня плата')) // true
-log(wallet.getOperationLenght()) // 1
-//20 Недостатньо коштів
-log(wallet.decrease(2000, 'Купити телефон')) // false
-log(wallet.getOperationLenght()) // 1
-log(wallet.decrease(500, 'Купити морозиво')) // true
-log(wallet.getOperationLenght()) // 2
-log(wallet.balance) // 500
-log(wallet.operations)
-
-
-
-
+user.f() // Квітка ( this == user )
+admin.f() // Пальчик ( this == admin )
