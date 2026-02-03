930bf40ddf252b6850b2f3a64d6cc36ff8afb567
kvitka <hoobarivka@gmail.com>
2026-02-03 19:48:54 +0200

    Функція-контструктор об`єкту, як спосіб створення великої кількоссті `однотипних об-єктів`

--- app.js
+++ app.js
@@ -1,48 +1,25 @@
 'use strict';
 
-const person = {
-    firstName: "John",
-    lastName: "Doe",
-    language: "",
-    get lang() {
-        return this.language
-    },
-    set lang(lang) {
-        this.language = lang.toUpperCase()
-    },
-    get fullName() {
-        return this.firstName + " " + this.lastName
+function Person(first, last, age, eye) {
+    this.firstName = first
+    this.lastName = last
+    this.age = age
+    this.eyeColor = eye
+    this.changeName = function(name) {
+        this.lastName = name
     }
-} 
+    //this.nationality = "English"
+}
 
-person.lang = "en"
-console.log(person.language)
-console.log(person.fullName)
+const myFather = new Person("John", "Doe", 50, "blue")
+const myMother = new Person("Sally", "Rally", 48, "green")
 
+myFather.nationality = "English"
+//Person.nationality = "English"
 
+myMother.changeName("Doe")
 
-const obj = { counter: 0 }
+console.log(Person)
 
-Object.defineProperty(obj, "reset", {
-    get: function() { this.counter = 0 }
-})
-Object.defineProperty(obj, "increment", {
-    get: function() { this.counter++}
-})
-Object.defineProperty(obj, "decrement", {
-    get: function() { this.counter--}
-})
-Object.defineProperty(obj, "add", {
-    set: function(value) { this.counter +=value }
-})
-Object.defineProperty(obj, "subtract", {
-    set: function(value) { this.counter -= value }
-})
-
-console.log(obj)
-console.log(obj.reset)
-console.log(obj.add = 5)
-console.log(obj.subtract = 1)
-console.log(obj.increment)
-console.log(obj.decrement)
-console.log(obj.add = 2)
+console.log(myMother)
+console.log(myFather)
