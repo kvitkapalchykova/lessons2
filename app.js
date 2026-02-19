70ec5f9b4653a3a6ce15257eeb7324ad44003b0d
kvitka <hoobarivka@gmail.com>
2026-02-19 20:55:09 +0200

    Вправи з датою та часом

--- app.js
+++ app.js
@@ -1,33 +1,34 @@
 'use strict';
 
-// автокорекція :
-let data = new Date(2013, 0, 32)// 32 січня не існує
-console.log(data) // це 1 лютого 2013
+// створити об’єкт Date на дату: 20 лютого 2012, 3:12 ранку (варіанти) :
+let d1 = new Date("2012-02-20T03:12")
+console.log(d1)
 
-// збільшимо дату на 2 дні :
-data = new Date(2016, 1, 28)
-data.setDate(data.getDate() + 2)
-console.log(data) // 1 березня 2016
-
-// збільшимо на 50 днів :
-data = new Date()
-data.setDate(data.getDate() + 50)
-console.log(data) // 10 квітня 2026
+let d2 = new Date(2012, 1, 20, 3, 12)
+console.log(d2)
 
 
-data = new Date(2016, 0, 2) // 2 січня 2016
-data.setDate(1) // встановити 1 день місяця
-console.log(data)
+// вивести день тижня літерами :
+let d3 = new Date(2012, 0, 3) // 3 січня 2012
 
-data.setDate(0)
-console.log(data) // 31 грудня, тому, що мін номер дня - 1,тому передбачається, що це останній день попереднього місяця
+function getWeekDay(date) {
+  const days = ["ПН", "ВТ", "СР", "ЧТ", "ПТ", "СБ", "НД"]
+  return days[date.getDay()]
+}
+console.log(getWeekDay(d3)) // СР
 
 
-data = new Date()
-console.log(+data) // об’єкт перетворюється на номер, він стає timestamp 
+// повернути “європейський” день тижня :
+let d4 = new Date(2012, 0, 3)
 
-// швидший метод отримання timestamp : 
-const d = Date.now()
-console.log(d)
+function getLocalDay(date) {
+  let day = date.getDay();
 
+  if (day == 0) { // 0 (неділя) 7 по-європейськи
+    day = 7;
+  }
+
+  return day;
+}
+console.log(getLocalDay(d4)) // вівторок, слід показати 2
 
