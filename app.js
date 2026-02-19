f47fe0b2b88193bce7681ccc4f370b8c3c73f412
kvitka <hoobarivka@gmail.com>
2026-02-19 14:59:06 +0200

    Об`єкт Date та його методи

--- app.js
+++ app.js
@@ -1,53 +1,63 @@
 'use strict';
 
-const prices = {
-  banana: 1,
-  orange: 2, 
-  meat: 4
-}
+const data = new Date()
 
-let doublePrices = Object.fromEntries(
-  // перетворити ціни на масив, потім застосувати map, щоб перетворити на пари ключ/значення :
-  // а потім fromEntries повертає об’єкт :
-  Object.entries(prices).map(entry => [entry[0], entry[1] * 2])
-)
+console.log(data) // показує поточну дату/час 
 
-console.log(doublePrices.meat) // 8
-console.log(doublePrices)
+// години у поточному часовому поясі :
+console.log(data.getHours())
 
+//  години у часовій зоні UTC+0 (Лондонський час без літніх переводів часу):
+console.log(data.getUTCHours())
 
+// різниця між UTC та місцевим часовим поясом, у хвилинах:
+console.log(data.getTimezoneOffset()) // -120
 
-const salaries = {
-  "Іван": 100,
-  "Петро": 300,
-  "Марія": 250
-}
+// Отримати рік (4 цифри) :
+console.log(data.getFullYear()) // 2026
 
-// отримаємо суму всіх зарплат за допомогою Object.values та циклуfor..of.
-function sumSalaries(salaries) {
+// Отримати місяць, від 0 до 11 :
+console.log(data.getMonth()) // 1 (лютий)
 
-  let sum = 0
-  for (let salary of Object.values(salaries)) {
-    sum += salary
-  } 
+// Отримати день місяця, від 1 до 31 :
+console.log(data.getDate()) 
 
-  return sum 
-}
+// день тижня, від 0 (неділя) до 6 (субота) :
+console.log(data.getDay()) 
 
-/* варіант 2 : за допомогою reduce перебираємо масив значень salaries,складаємо їх і повертаємо результат :*/
-function sumSalaries(salaries) {
-  return Object.values(salaries).reduce((a, b) => a + b, 0) 
-}
+// отримати годину, хвилини, секунди, мілісекунди :
+console.log(data.getHours())
+console.log(data.getMinutes())
+console.log(data.getSeconds())
+console.log(data.getMilliseconds())
 
-console.log(sumSalaries(salaries)) // 650
+// повертає кількість мілісекунд з 1 січня 1970 року :
+console.log(data.getTime())
 
 
-const user = {
-  name: 'Іван',
-  age: 30
-}
-// визначити кількість властивостей об'єкта :
-function count(obj) {
-  return Object.keys(obj).length
-}
-console.log(count(user)) // 2
+
+// 0 означає 01.01.1970 UTC+0 :
+let Jan01_1970 = new Date(0)
+console.log(Jan01_1970)
+
+// тепер додамо 24 години, отримаємо 02.01.1970 UTC+0 :
+let Jan02_1970 = new Date(24 * 3600 * 1000)
+console.log(Jan02_1970)
+
+// 31 грудня 1969 року :
+let Dec31_1969 = new Date(-24 * 3600 * 1000)
+console.log(Dec31_1969)
+
+
+// Один день (24 години) - це 86 400 000 мілісекунд :
+const d = new Date(86400000)
+console.log(d)
+// Fri Jan 02 1970 03:00:00 GMT+0300 (за східноєвропейським стандартним часом) 
+
+
+let now = new Date("2017-01-26")
+now = new Date(2011, 0, 1, 0, 0, 0, 0)
+now = new Date(2011, 0, 1)
+now = new Date(2011, 0, 1, 2, 3, 4, 567)
+console.log(now)
+
